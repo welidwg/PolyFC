0cc1b74971ecb787fd0000aababf38232ba60497
welidwg <walidgueddari1899@gmail.com>
2022-12-27 16:28:15 +0100

    wg

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -11,11 +12,8 @@ class UserController extends AbstractController
     /**
      * @Route("/user", name="app_user")
      */
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+       return $this->render("base.html.twig");
     }
 }
