e30267e00fd64d2a3844b9aebdeff4d09ff9aa90
welidwg <walidgueddari1899@gmail.com>
2022-12-28 00:40:34 +0100

    wg

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -145,7 +146,8 @@ class UserController extends AbstractController
             $password = $passwordEncoder->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
             $em = $this->getDoctrine()->getManager();
-
+            $seesion = new Session();
+            $seesion->getFlashBag()->add("message", ["type" => "success", "content" => "Candidat bien ajouté"]);
             $user->setRoles(array("ROLE_TEACHER"));
             $user->setActif(0);
             $em->persist($user);
