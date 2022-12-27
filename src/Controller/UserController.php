f8649647e3b0b3e63b01bce17653a9c483dfe5f9
welidwg <walidgueddari1899@gmail.com>
2022-12-27 16:34:02 +0100

    wg

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,11 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="main")
+     */
+    public function main(): Response
+    {
+        return $this->render("main/index.html.twig");
+    }
     /**
      * @Route("/user", name="app_user")
      */
     public function index(): Response
     {
-       return $this->render("base.html.twig");
+        return $this->render("base.html.twig");
     }
 }
