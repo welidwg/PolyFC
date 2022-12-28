51365d707d1d7b0674070c9ef202fa7c16b72c07
welidwg <walidgueddari1899@gmail.com>
2022-12-28 20:33:59 +0100

    wg

--- src/Controller/CertifController.php
+++ src/Controller/CertifController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Certification;
+use App\Entity\UserCertif;
 use App\Form\CertificationType;
 use App\Repository\CertificationRepository;
+use App\Repository\UserCertifRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -81,9 +83,43 @@ class CertifController extends AbstractController
      */
     public function delete(Request $request, Certification $certification, CertificationRepository $certificationRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$certification->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $certification->getId(), $request->request->get('_token'))) {
             $certificationRepository->remove($certification, true);
         }
+
+        return $this->redirectToRoute('app_certif_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    /**
+     * @Route("/userCertif/{id}", name="app_certif_usercertif", methods={"POST","GET"})
+     */
+    public function userCertif(Request $request, UserCertifRepository $userCertifRepository, CertificationRepository $certificationRepository, $id): Response
+    {
+        $userCertif = new UserCertif();
+        $certif = $certificationRepository->find($id);
+
+        $userCertif->setCertif($certif);
+        $userCertif->setUser($this->getUser());
+        $userCertif->setDemande(0);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($userCertif);
+        $em->flush();
+
+        return $this->redirectToRoute('app_certif_index', [], Response::HTTP_SEE_OTHER);
+    }
+    /**
+     * @Route("/userCertif/cancel/{id}", name="app_certif_usercertif_cancel", methods={"POST","GET"})
+     */
+    public function userCertifCancel(Request $request, UserCertifRepository $userCertifRepository, $id): Response
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $userCert = $userCertifRepository->find($id);
+        if ($userCert) {
+            $em->remove($userCert);
+            $em->flush();
+        }
+
 
         return $this->redirectToRoute('app_certif_index', [], Response::HTTP_SEE_OTHER);
     }
